ff9336fdbea9a5db81548a30abdeed3a4f63b5df
inwf <www.inwffffff@outlook.com>
2025-11-17 15:20:51 +0800

    初始化

---
+++ cn/TwoSum.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+/**
+ * 1 两数之和
+ * 2025-11-17 15:18:52
+ * inwf
+ */
+
+public class TwoSum {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            //11
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+        // put your test code here
+        solution.twoSum(   );
+    }
+}
