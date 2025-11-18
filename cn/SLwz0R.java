20b2fcb2653ee92370eb7d8dc5ed974ef6e0e73a
inwf <inwffffff@outlook.com>
2025-11-18 17:56:06 +0800

    19 删除链表的倒数第 N 个结点

---
+++ cn/SLwz0R.java
@@ -0,0 +1,54 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+import leetcode.editor.common.*;
+
+/**
+ * LCR 021 删除链表的倒数第 N 个结点
+ * 2025-11-18 17:42:00
+ * inwf
+ */
+
+public class SLwz0R {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummyHead = new ListNode(-1, head);
+            ListNode fast = dummyHead;
+            ListNode slow = dummyHead;
+
+            for (int i = 0; i < n; i++) {
+                fast = fast.next;
+            }
+
+            while (fast.next != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            slow.next = slow.next.next;
+            return dummyHead.next;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+    public static void main(String[] args) {
+        Solution solution = new SLwz0R().new Solution();
+        // put your test code here
+        // solution.
+    }
+}
