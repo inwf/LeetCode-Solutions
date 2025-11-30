0dde38ecc82a8150f46fdd10d1af6be76963a0bc
inwf <inwffffff@outlook.com>
2025-11-30 19:02:36 +0800

    141 环形链表

---
+++ cn/LinkedListCycle.java
@@ -0,0 +1,51 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+/**
+ * 141 环形链表
+ * 2025-11-30 19:00:04
+ * inwf
+ */
+
+public class LinkedListCycle {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            // 快慢指针
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while(fast!=null && fast.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+                if (fast == slow){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new LinkedListCycle().new Solution();
+        // put your test code here
+        // solution.
+    }
+}
