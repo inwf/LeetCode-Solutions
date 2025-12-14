2bd13792ced6154f3fbac633d98322ccd3533893
inwf <inwffffff@outlook.com>
2025-12-14 18:54:34 +0800

    560 和为 K 的子数组

--- cn/SubarraySumEqualsK.java
+++ cn/SubarraySumEqualsK.java
@@ -15,21 +15,21 @@ public class SubarraySumEqualsK {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int subarraySum(int[] nums, int k) {
-            int n = nums.length;
-            int ans = 0;
+            // 子数组是连续的
 
-            // 前缀和，sum[i] 表示 nums[0] + ... + nums[i-1] 之和
+            int ans = 0;
+            int n = nums.length;
+            // s[i] 表示 nums[0,i - 1] 之和
             int[] sum = new int[n + 1];
             for (int i = 0; i < n; i++) {
                 sum[i + 1] = sum[i] + nums[i];
             }
 
-            //  cnt[i] 表示和为 i 的个数
             Map<Integer, Integer> cnt = new HashMap<>();
             for (int i = 0; i < sum.length; i++) {
                 ans += cnt.getOrDefault(sum[i] - k, 0);
-                // 为什么先 ans+ 再 put？
-                // 因为判断的是 sum[i] 前面有几个和为 sum[i] - k 的子数组
+                // 因为是非空的，所以必须先算 ans 再把 sum[0] put 进去
+                // 反例： nums = {1}，k = 0
                 cnt.put(sum[i], cnt.getOrDefault(sum[i], 0) + 1);
             }
 
@@ -44,5 +44,4 @@ public class SubarraySumEqualsK {
         // put your test code here
         solution.subarraySum(new int[]{1, 1, 1}, 2);
     }
-
 }
