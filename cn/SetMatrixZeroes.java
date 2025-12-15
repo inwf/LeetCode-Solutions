61dc41fe61de045f21c7956a1fe48b28ec8c9d55
inwf <inwffffff@outlook.com>
2025-12-15 19:31:23 +0800

    73 矩阵置零

--- cn/SetMatrixZeroes.java
+++ cn/SetMatrixZeroes.java
@@ -15,30 +15,27 @@ public class SetMatrixZeroes {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void setZeroes(int[][] matrix) {
-            // 考虑把数据保存到 matrix 的第一行和第一列中。类似 Excel 表格的第一行和第一列，保存汇总信息
-            // 参考灵神
-            // 总结：用第一列和第一行表示该列或者该行需不需要全为 0
-            // 第一行第一列单独判断
-            int m = matrix.length;
-            int n = matrix[0].length;
+            // 从左上角开始，第一行第一列单独判断
             boolean row = false;
             boolean col = false;
+            int m = matrix.length;
+            int n = matrix[0].length;
 
-            // 判断第一行有没有 0
+            // 第一行
             for (int i = 0; i < n; i++) {
                 if (matrix[0][i] == 0) {
                     row = true;
-                    break;
-                }
-            }
-            // 判断第一列有没有 0
-            for (int i = 0; i < m; i++) {
-                if (matrix[i][0] == 0) {
-                    col = true;
-                    break;
                 }
             }
 
+            // 第一列
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][0] == 0) {
+                    col = true;
+                }
+            }
+
+            // 开始遍历，然后标记到第一行和第一列
             for (int i = 1; i < m; i++) {
                 for (int j = 1; j < n; j++) {
                     if (matrix[i][j] == 0) {
@@ -48,15 +45,7 @@ public class SetMatrixZeroes {
                 }
             }
 
-            // 把所有有 0 的列置 0
-            for (int i = 1; i < n; i++) {
-                if (matrix[0][i] == 0) {
-                    for (int j = 0; j < m; j++) {
-                        matrix[j][i] = 0;
-                    }
-                }
-            }
-            // 把所有有 0 的行置 0
+            // 开始根据标记行染色
             for (int i = 1; i < m; i++) {
                 if (matrix[i][0] == 0) {
                     for (int j = 0; j < n; j++) {
@@ -65,17 +54,29 @@ public class SetMatrixZeroes {
                 }
             }
 
-            // 单独判断第一行和第一列
+            // 开始根据标记列染色
+            for (int i = 1; i < n; i++) {
+                if (matrix[0][i] == 0) {
+                    for (int j = 0; j < m; j++) {
+                        matrix[j][i] = 0;
+                    }
+                }
+            }
+
+            // 第一行需要染色
             if (row) {
                 for (int i = 0; i < n; i++) {
                     matrix[0][i] = 0;
                 }
             }
+
+            // 第一列需要染色
             if (col) {
                 for (int i = 0; i < m; i++) {
                     matrix[i][0] = 0;
                 }
             }
+
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
