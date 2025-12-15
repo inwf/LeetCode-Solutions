9799f4be23e54eb42da568d04ee1b211fe6cab56
inwf <inwffffff@outlook.com>
2025-12-15 14:36:17 +0800

    41 缺失的第一个正数

--- cn/FirstMissingPositive.java
+++ cn/FirstMissingPositive.java
@@ -15,20 +15,22 @@ public class FirstMissingPositive {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int firstMissingPositive(int[] nums) {
-            // 换座位：https://leetcode.cn/problems/first-missing-positive/solutions/3655377/huan-zuo-wei-tong-guo-li-zi-li-jie-suan-qa94e/?envType=study-plan-v2&envId=top-100-liked
+            // 换座位
+            // 参考：https://leetcode.cn/problems/first-missing-positive/solutions/3655377/huan-zuo-wei-tong-guo-li-zi-li-jie-suan-qa94e/?envType=study-plan-v2&envId=top-100-liked
             // 如果当前元素值为 x，那么 x 应该在下标为 x - 1 的位置
             // nums[i] == nums[nums[i] - 1]
+
             int n = nums.length;
             for (int i = 0; i < n; i++) {
-                // 忽略负数、0、以及大于 len 的数
-                // 通过 nums[nums[i] - 1] != nums[i] 可以判断出影分身和本体（参考力扣灵神）
-                while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // for 循环一轮后，nums 中每个数字都到了自己应该在的位置
+                while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                     int j = nums[i] - 1;
                     swap(nums, i, j);
                 }
             }
 
             for (int i = 0; i < n; i++) {
+                // 这里已经换好座位了，所以直接判断
                 if (nums[i] != i + 1) {
                     return i + 1;
                 }
@@ -42,8 +44,6 @@ public class FirstMissingPositive {
             nums[i] = nums[j];
             nums[j] = temp;
         }
-
-
     }
     //leetcode submit region end(Prohibit modification and deletion)
 
