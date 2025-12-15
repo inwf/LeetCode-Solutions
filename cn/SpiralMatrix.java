d1e9eea37c1b53d26ff6956a1f164a6c416e8e0d
inwf <inwffffff@outlook.com>
2025-12-15 21:08:20 +0800

    54 螺旋矩阵

--- cn/SpiralMatrix.java
+++ cn/SpiralMatrix.java
@@ -15,34 +15,32 @@ public class SpiralMatrix {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<Integer> spiralOrder(int[][] matrix) {
-            // 模拟题
+            // 模拟题，标记走过的位置
+            // 走到不能走了就换方向
             List<Integer> ans = new ArrayList<>();
-            int m = matrix.length;
-            int n = matrix[0].length;
             int[] dx = {0, 1, 0, -1};
             int[] dy = {1, 0, -1, 0};
 
-            // 从 [0,0] 开始
-            int i = 0;
-            int j = 0;
+            int m = matrix.length;
+            int n = matrix[0].length;
+
             int idx = 0;
-            // 一个 m * n 个元素
-            for (int cnt = 0; cnt < m * n; cnt++) {
-                ans.add(matrix[i][j]);
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < m * n; i++) {
+                ans.add(matrix[x][y]);
+                matrix[x][y] = Integer.MAX_VALUE;
 
-                // 表示这个点被遍历过了
-                matrix[i][j] = Integer.MAX_VALUE;
+                int nx = x + dx[idx];
+                int ny = y + dy[idx];
 
-                int nx = i + dx[idx];
-                int ny = j + dy[idx];
                 if (nx < 0 || nx >= m || ny < 0 || ny >= n || matrix[nx][ny] == Integer.MAX_VALUE) {
                     idx = (idx + 1) % 4;
                 }
 
-                i = i + dx[idx];
-                j = j + dy[idx];
+                x = x + dx[idx];
+                y = y + dy[idx];
             }
-
             return ans;
         }
     }
