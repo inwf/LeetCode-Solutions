b592aabd647995b76e183a14ea7a932578bdf5f6
inwf <inwffffff@outlook.com>
2025-12-15 14:11:46 +0800

    56 合并区间

--- cn/MergeIntervals.java
+++ cn/MergeIntervals.java
@@ -15,28 +15,25 @@ public class MergeIntervals {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[][] merge(int[][] intervals) {
-            // 模拟题
-            // 先排序（左端点排序）
-            // 然后遍历模拟即可：前一个节点的右端点 >= 当前节点的左端点，就合并
-            // 排序
-            Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+            // 先排序，然后模拟题
+
             int n = intervals.length;
             List<int[]> ans = new ArrayList<>();
+            Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
             for (int i = 0; i < n; i++) {
                 int[] cur = intervals[i];
+
                 if (i == 0) {
                     ans.add(cur);
+                    continue;
+                }
+
+                int[] pre = ans.get(ans.size() - 1);
+                if (pre[1] >= cur[0]) {
+                    pre[1] = Math.max(pre[1], cur[1]);
                 } else {
-                    int[] last = ans.get(ans.size() - 1);
-                    if (last[1] >= cur[0]) {
-                        // 可以合并（数组是引用类型）
-                        last[1] = Math.max(last[1], cur[1]);
-//                        ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], cur[1]);
-                    } else {
-                        // 不能合并
-                        ans.add(cur);
-                    }
+                    ans.add(cur);
                 }
             }
 
