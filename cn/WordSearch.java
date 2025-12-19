50a4ee40e524523b5dbc9c1f5345c43eb21e5c23
inwf <inwffffff@outlook.com>
2025-12-19 20:17:12 +0800

    79 单词搜索

--- cn/WordSearch.java
+++ cn/WordSearch.java
@@ -14,15 +14,16 @@ public class WordSearch {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        int m = 0;
+        int n = 0;
         int[] dx = {0, 1, 0, -1};
         int[] dy = {1, 0, -1, 0};
-        int m;
-        int n;
 
         public boolean exist(char[][] board, String word) {
             // 遍历所有开始，然后 dfs 枚举匹配
             m = board.length;
             n = board[0].length;
+
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (dfs(0, i, j, board, word)) {
@@ -34,32 +35,35 @@ public class WordSearch {
             return false;
         }
 
-        public boolean dfs(int k, int x, int y, char[][] board, String word) {
-            // k 是当前匹配的 word 的下标
-            if (word.charAt(k) != board[x][y]) {
+        public boolean dfs(int idx, int x, int y, char[][] board, String word) {
+            // idx 是当前 word 的下标
+            if (board[x][y] != word.charAt(idx)) {
                 return false;
             }
 
-            // 当前位置匹配：board[x][y] == word[k]
-            // k 是下标！！！
-            if (k == word.length() - 1) {
+            // 这里 [x,y] 已经匹配了 word[idx]
+            if (idx == word.length() - 1) {
                 return true;
             }
 
-            // 标记访问过
+            // 标记这个点已经走过了
             board[x][y] = '#';
+
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (0 <= nx && nx < m && 0 <= ny && ny < n) {
-                    if (dfs(k + 1, nx, ny, board, word)) {
-                        return true;
-                    }
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                    continue;
+                }
+
+                if (dfs(idx + 1, nx, ny, board, word)) {
+                    return true;
                 }
             }
+
             // 恢复现场
-            board[x][y] = word.charAt(k);
-            // 如果当前位置往哪边走都不匹配的话，就 return false
+            board[x][y] = word.charAt(idx);
+
             return false;
         }
     }
