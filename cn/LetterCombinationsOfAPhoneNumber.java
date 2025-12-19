5581665a434667350395bced96a637c77aa39cce
inwf <inwffffff@outlook.com>
2025-12-19 18:24:39 +0800

    17 电话号码的字母组合

--- cn/LetterCombinationsOfAPhoneNumber.java
+++ cn/LetterCombinationsOfAPhoneNumber.java
@@ -15,8 +15,8 @@ public class LetterCombinationsOfAPhoneNumber {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         List<String> ans = new ArrayList<>();
-        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         StringBuilder path = new StringBuilder();
+        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wzyx"};
 
         public List<String> letterCombinations(String digits) {
             // 回溯 + 剪枝
@@ -25,7 +25,7 @@ public class LetterCombinationsOfAPhoneNumber {
         }
 
         public void dfs(String digits, int idx) {
-            // idx 代表遍历到的 digits 的下标
+            // idx 是当前遍历到哪一个下标了，刚开始是 0 表示从 0 开始选择选哪个字母
             if (idx == digits.length()) {
                 ans.add(path.toString());
                 return;
@@ -33,10 +33,11 @@ public class LetterCombinationsOfAPhoneNumber {
 
             String cur = map[digits.charAt(idx) - '0'];
             for (int i = 0; i < cur.length(); i++) {
-                // 选择当前字母
-                path.append(cur.charAt(i));
+                // 选择 c
+                char c = cur.charAt(i);
+                path.append(c);
 
-                // 遍历下一个数字，开始下标是 idx + 1
+                // 进入选完 c 后的 dfs
                 dfs(digits, idx + 1);
 
                 // 恢复现场
