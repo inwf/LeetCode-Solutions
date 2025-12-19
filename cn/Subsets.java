e2d25105d2cf7b0b68178a7198950f2162133e7f
inwf <inwffffff@outlook.com>
2025-12-19 13:33:51 +0800

    78 子集

--- cn/Subsets.java
+++ cn/Subsets.java
@@ -18,7 +18,7 @@ public class Subsets {
         List<Integer> path = new ArrayList<>();
 
         public List<List<Integer>> subsets(int[] nums) {
-            // 比常规递归简单
+            // 无序：取过的元素不会重复取，写回溯算法的时候，for 就要从 startIndex 开始
             // 子集问题会收集所有节点的结果
             // 所以不需要判断是否到叶子节点，直接收集答案即可
             dfs(nums, 0);
@@ -26,19 +26,21 @@ public class Subsets {
         }
 
         public void dfs(int[] nums, int startIndex) {
+            // 每一次进 dfs 都 add
             ans.add(new ArrayList<>(path));
 
-            if (startIndex == nums.length) {
+            if (startIndex >= nums.length) {
                 return;
             }
 
             for (int i = startIndex; i < nums.length; i++) {
-                // 选 nums [i]
+                // 选择 nums[i]
                 path.add(nums[i]);
 
                 // 进入选完 nums[i] 后的 dfs
                 dfs(nums, i + 1);
 
+                // 恢复现场
                 path.remove(path.size() - 1);
             }
         }
