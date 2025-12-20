553e30f94694fc9af5d82e8cec68c5ada838ab74
inwf <inwffffff@outlook.com>
2025-12-20 14:12:41 +0800

    35 搜索插入位置

--- cn/SearchInsertPosition.java
+++ cn/SearchInsertPosition.java
@@ -16,15 +16,18 @@ public class SearchInsertPosition {
     class Solution {
         public int searchInsert(int[] nums, int target) {
             // 典型二分
-            // 找到第一个 >= target 元素的下标
+            // 找到第一个 nums[mid] >= target 元素的下标
 
-            int l = 0, r = nums.length - 1;
-            int ans = nums.length;
+            int l = 0;
+            int r = nums.length - 1;
+            int ans = nums.length; // 没找到就返回数组长度，这里的初始化不是随便来的！
+
             while (l <= r) {
                 int mid = l + (r - l) / 2;
+
                 if (nums[mid] >= target) {
-                    r = mid - 1;
                     ans = mid;
+                    r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
