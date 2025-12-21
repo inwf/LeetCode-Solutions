e1d27abdcee6b1c83b088c68bb9aaa4292d5a0ac
inwf <inwffffff@outlook.com>
2025-12-21 23:55:22 +0800

    118 杨辉三角

--- cn/PascalsTriangle.java
+++ cn/PascalsTriangle.java
@@ -16,19 +16,25 @@ public class PascalsTriangle {
     class Solution {
         public List<List<Integer>> generate(int numRows) {
             // 每一行下标从 1 开始，其他地方就是 0
-            List<List<Integer>> ans = new ArrayList<>(numRows);
+            // 每个位置的值是该位置上面和左上方的值之和
+            // 行数范围：[0, numRows]
+
+            List<List<Integer>> ans = new ArrayList<>();
+            // 第 0 行
             ans.add(List.of(1));
+
             for (int i = 1; i < numRows; i++) {
                 List<Integer> row = new ArrayList<>(i + 1);
-                // 每行开头是 1
+                // 每一行第一个是 1
                 row.add(1);
+
                 for (int j = 1; j < i; j++) {
                     row.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
                 }
-                ans.add(row);
 
-                // 结尾是 1
+                // 每一行最后一个是 1
                 row.add(1);
+                ans.add(row);
             }
             return ans;
         }
