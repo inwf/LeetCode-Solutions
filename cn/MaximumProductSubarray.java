d94bd38dfbd15191339a3478c38d0be56f507d1e
inwf <inwffffff@outlook.com>
2025-12-22 18:13:52 +0800

    152 乘积最大子数组

--- cn/MaximumProductSubarray.java
+++ cn/MaximumProductSubarray.java
@@ -18,24 +18,19 @@ public class MaximumProductSubarray {
             // dp[i] 表示 nums[i] 结尾的最大乘积
 
             int n = nums.length;
-            if (n == 1) {
-                return nums[0];
-            }
-
-            // 记录最大和最小（负数）
+            // 本次乘积最大可能在下次变成乘积最小
             int[] dpMax = new int[n];
             int[] dpMin = new int[n];
             dpMax[0] = nums[0];
             dpMin[0] = nums[0];
             int ans = nums[0];
-
             for (int i = 1; i < n; i++) {
                 int x = nums[i];
-                dpMax[i] = Math.max(Math.max(dpMax[i - 1] * x, dpMin[i - 1] * x), x);
-                dpMin[i] = Math.min(Math.min(dpMax[i - 1] * x, dpMin[i - 1] * x), x);
-                ans = Math.max(ans, dpMax[i]);
+                // 乘与不乘
+                dpMax[i] = Math.max(x,(Math.max(dpMax[i - 1] * nums[i], dpMin[i - 1] * nums[i])));
+                dpMin[i] = Math.min(x,(Math.min(dpMax[i - 1] * nums[i], dpMin[i - 1] * nums[i])));
+                ans = Math.max(ans,(Math.max(dpMax[i], dpMin[i])));
             }
-
             return ans;
         }
     }
