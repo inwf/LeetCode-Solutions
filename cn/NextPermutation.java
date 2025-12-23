2c0d0c8293a7045c2d94a55c85b55a1efcc6a650
inwf <inwffffff@outlook.com>
2025-12-23 19:07:18 +0800

    31 下一个排列

--- cn/NextPermutation.java
+++ cn/NextPermutation.java
@@ -16,30 +16,32 @@ public class NextPermutation {
     class Solution {
         public void nextPermutation(int[] nums) {
             // 参考：https://leetcode.cn/problems/next-permutation/solutions/80560/xia-yi-ge-pai-lie-suan-fa-xiang-jie-si-lu-tui-dao-/?envType=study-plan-v2&envId=top-100-liked
+            // 如果找不到,就返回升序（找不到的情况是原始就是倒序，reverse 即可）
             int n = nums.length;
 
-            int i;
+            // 先找出第一个递增的位置
+            int i = 0;
             for (i = n - 2; i >= 0; i--) {
-                // 从右往找到第一个 nums[i] < nums[i+1] 的位置
                 if (nums[i] < nums[i + 1]) {
                     break;
                 }
             }
 
-            // 找到了，从右往左找第一个大于 nums[i] 的元素
             if (i >= 0) {
+                // 找到了
+                // 继续找最小的比 nums[i] 大的数，然后调换
                 int j = n - 1;
                 while (nums[j] <= nums[i]) {
                     j--;
                 }
 
-                // swap(nums,i,j);
+                // 此时 nums[j] 是 nums[i] 右边最小的 > nums[i] 的数
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
             }
 
-            // 将 nums[i] 之后的元素改为递增
+            // 最后将 i 后面改成递增即可
             reverse(nums, i + 1, n - 1);
         }
 
