2fa536c8c444f9b773d897feeac516f33c17b3d0
inwf <inwffffff@outlook.com>
2025-12-25 21:58:07 +0800

    287 寻找重复数

--- cn/FindTheDuplicateNumber.java
+++ cn/FindTheDuplicateNumber.java
@@ -17,26 +17,29 @@ public class FindTheDuplicateNumber {
         public int findDuplicate(int[] nums) {
             // 环形链表变式
             // https://leetcode.cn/problems/find-the-duplicate-number/solutions/3797843/yong-ji-huan-shu-li-jie-zuo-fa-tong-142-tkoc2/?envType=study-plan-v2&envId=top-100-liked
-            int fast = 0;
-            int slow = 0;
+            // 不用纠结，题目是精心设计的 1 <= nums[i] <= n
+            // 所以从下标 0 进去，不可能走到 nums[i] = i 情况
+
+            int fast = nums[0];
+            int slow = nums[0];
 
             while (true) {
                 fast = nums[nums[fast]];
                 slow = nums[slow];
 
                 if (fast == slow) {
+                    // 相交
                     break;
                 }
             }
 
-            int temp = 0;
+            int temp = nums[0];
             while (temp != slow) {
                 temp = nums[temp];
                 slow = nums[slow];
             }
 
-            // 当前元素是 slow，下一个元素是 nums[slow]
-            return slow;
+            return temp;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
